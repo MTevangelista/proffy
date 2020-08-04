58aaf8c1fb05b86eb958baa3e2c33f8f1153e03f
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-04 19:48:48 -0300

    Server: Add route to create a class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' })
-})
+app.use(routes)
 
 const port = 3333
 app.listen(port, () => {
