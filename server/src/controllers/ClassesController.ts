f27369629821665e793b8b9beebd5217090b0f17
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-08-05 09:33:43 -0300

    Server: Create connections

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -81,7 +81,7 @@ export default class ClassesController {
             return res.status(201).send()
         } catch (err) {
             await trx.rollback()
-    
+            
             return res.status(400).json({
                 error: 'Unexpected error while creating a new class'
             })
